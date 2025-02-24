cb916f767e98ef17c5794fb53a6e45fab19db8bf
CzeleHub <szymondyktyserviam@gmail.com>
2025-02-24 15:21:32 +0100

    update readme, small changes to main.rs

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-TopRSS
+# TopRSS
 
-Command line utility for printing top VmRSS processes
+Linux Command line utility for printing top VmRSS processes
 
-It is Linux only, since it is designed around VmRSS and /proc directory
+This program was designed in mind with integration to status bars like WayBar[https://github.com/Alexays/Waybar/] or Gnome Top Bar with Executor[https://extensions.gnome.org/extension/2932/executor/]
 
-This program was designed in mind with integrationto to some bars, such as WayBar or Executor for Gnome
+There is no official release yet
 
-It is not intended for usage yet
+# Installation
+
+To compile (on linux) follow these steps:
+1. (install rust)[https://www.rust-lang.org/tools/install]
+2. $:```git clone ```
+3. cargo build --release
--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ fn main() {
                     return;
                 }
             }
+            "--group" => {} // since its the default behaviour there is nothing to do
             "--lines" => {
                 layout = Layout::Lines;
             }
@@ -103,7 +104,7 @@ Command line utility for printing top VmRSS processes
 options:
   -h, --help, -H, -?                 display this help message and exit
   -v, --version                      display program's version and exit
-  -g, --group        DEFAULT         group processes with the same name //to be implemented
+  -g, --group        DEFAULT         group processes with the same name
   -u, --ungroup                      ungroup processes with the same name //to be implemented
   -n,                DEFAULT n = 3   display at most top 'n' processes
   -a, --all                          display all processes
